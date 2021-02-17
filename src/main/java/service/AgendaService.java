11852909e4bd978d460c1d635c6b2f94fe636b7e
Geta Bugnar <bugnargeta@gmail.com>
2021-02-17 12:13:07 -0800

    adding AgendaService

---
+++ src/main/java/service/AgendaService.java
@@ -0,0 +1,41 @@
+package service;
+
+import org.example.domain.Agenda;
+import persistance.AgendaRepository;
+import transfer.AgendaNameRequest;
+import transfer.AgendaRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class AgendaService {
+
+    private AgendaRepository agendaRepository = new AgendaRepository();
+
+
+    public void createAgenda(AgendaRequest request) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Creating agenda:" + request);
+        agendaRepository.createAgenda(request);
+    }
+
+    public void updateAgenda(AgendaRequest request, long id) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Updating agenda: " + request + id);
+        agendaRepository.updateAgenda(request, id);
+    }
+
+    public void deleteAgenda(long id) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Deleting agenda :" + id);
+        agendaRepository.deleteAgenda(id);
+    }
+
+    public List<Agenda> getAgenda() throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Retrieving all agenda.");
+        return agendaRepository.getAgenda();
+    }
+
+    public List<Agenda> getAgendaofName(AgendaNameRequest request) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Retrieving  agenda: " + request);
+        return agendaRepository.getAgendaofName(request);
+    }
+}
